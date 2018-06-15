213a9020ac550a24c0031a9af5d500763823c3c1
naman <namanchopra21087@gmail.com>
2018-06-15 16:26:53 +0530

    Committing missing mock-heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES:Hero[]=[
+    {id:11,name:"Goblins"},
+    {id:12,name:"Pegasus"},
+    {id:13,name:"Chocobo"},
+    {id:14,name:"Sophia"},
+    {id:15,name:"Apollo"},
+    {id:16,name:"Tiger"},
+    {id:17,name:"Lions"},
+    {id:18,name:"Cubs"},
+    {id:19,name:"Caching"},
+    {id:20,name:"Caught"}
+]
